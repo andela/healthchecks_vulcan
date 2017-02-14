fb963422e39812385b2b7c5fa1d0a2a9f4f8f6ec
brayovsky <brian.muthui@andela.com>
2017-02-14 19:55:36 +0300

    Remove added heroku lines in settings.py and wsgi.py

--- hc/wsgi.py
+++ hc/wsgi.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 # import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
 #----------------Heroku Sh*t------------------#
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
