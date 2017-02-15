42936fff69fe41b8113291baa778e42daff7f119
brayovsky <brian.muthui@andela.com>
2017-02-15 14:08:50 +0300

    [chore #139617795] Prepare files for heroku upload -Add correct app name in Procfile - Add settings for heroku in settings.py - Change application in wsgi.py

--- hc/wsgi.py
+++ hc/wsgi.py
@@ -15,9 +15,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
 
 application = get_wsgi_application()
 
-#----------------Heroku Sh*t------------------#
+#----------------Heroku Stuff——————————————————#
 
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-# application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
