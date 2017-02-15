498ce780a00f897ddc49b38b1bc11552609e8d4a
brayovsky <brian.muthui@andela.com>
2017-02-15 14:26:00 +0300

    [chore #139617795] Prepare files for heroku upload - Remove variables BASE_DIR,STATIC_URL,STATICFILES_DIR and STATIC_ROOT for heroku

--- hc/settings.py
+++ hc/settings.py
@@ -167,11 +167,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
