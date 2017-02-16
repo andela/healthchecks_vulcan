30f1341355d576ed8378e9f74586fa38b4d806b6
brayovsky <brian.muthui@andela.com>
2017-02-16 18:28:39 +0300

    [chore #139617795] Configure email in heroku - Change host and site root to healthchecks-vulcan.herokuapp.com

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -11,8 +11,10 @@ DATABASES = {
 }
 
 DJMAIL_REAL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+SITE_ROOT = 'http://healthchecks-vulcan.herokuapp.com'
+HOST = 'healthchecks-vulcan.herokuapp.com'
