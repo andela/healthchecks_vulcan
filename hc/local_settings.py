83a8574683671377fb584deaf7e0e1eaad9b91a7
brayovsky <brian.muthui@andela.com>
2017-02-16 18:07:26 +0300

    [chore #139617795] Configure email in heroku - Add environment variables for email host, user, password, tls and port for gmail

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -1,3 +1,5 @@
+import os
+
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
@@ -7,3 +9,10 @@ DATABASES = {
         'TEST': {'CHARSET': 'UTF8'}
     }
 }
+
+DJMAIL_REAL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
