32a2dadb5bc79c69627291cfb9ba21ecadcfc4fb
Daniel sumba <dansumba@Daniels-MacBook-Pro.local>
2017-02-21 15:30:15 +0300

    Fix the badge test.. It test for bad signatures

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -12,8 +12,10 @@ class BadgeTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
     def test_it_rejects_bad_signature(self):
+        import ipdb; ipdb.set_trace()
+        
         r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
-        ### Assert the expected response status code
+        assert r.status_code == 400
 
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
@@ -21,4 +23,4 @@ class BadgeTestCase(BaseTestCase):
         url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
 
         r = self.client.get(url)
-        ### Assert that the svg is returned
+        self.assertContains(r, "#4c1")
