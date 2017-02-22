8c980e6eee759e3a20b9b9156b0c651bb7dc0e86
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-02-22 09:56:10 +0300

    [Feature

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -12,3 +12,9 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
+    def test_team_access_works(self):
+        url = "/checks/add/"
+        self.client.login(username="bob@example.org", password="password")
+        r = self.client.post(url)
+        self.assertRedirects(r, "/checks/")
+        assert Check.objects.count() == 1
