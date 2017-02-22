8a958565778d27c9999786044c70ef81069c486c
brayovsky <brian.muthui@andela.com>
2017-02-22 18:40:54 +0300

    [#139551015]Fix api tests - Add tests for superuser in admin tests - Add tests for pausing pings - Add tests for ensuring triggers - Add tests for pausing pings via api functionality

--- hc/api/tests/test_ensuretriggers.py
+++ hc/api/tests/test_ensuretriggers.py
@@ -16,14 +16,17 @@ class EnsureTriggersTestCase(TestCase):
         assert check.alert_after is None
 
         check.last_ping = timezone.now()
+
         check.save()
         check.refresh_from_db()
         assert check.alert_after is not None
-        ### The above assert fails. Make it pass
+        ### The above assert fails. Make it pass -> This test is ok!?!
 
         alert_after = check.alert_after
 
         check.last_ping += timedelta(days=1)
         check.save()
         check.refresh_from_db()
-        ### Assert that alert_after is lesser than the check's alert_after 
+        ### Assert that alert_after is lesser than the check's alert_after
+
+        assert alert_after < check.alert_after
