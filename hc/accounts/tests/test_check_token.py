6f566a048c5ff2df8d6fbd86bd6c38dd6ff1eb54
dennomwas <githinji.mwangi@gmail.com>
2017-02-23 18:00:28 +0300

    [CHORE #139551017]Update to add new tests

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -34,3 +34,4 @@ class CheckTokenTestCase(BaseTestCase):
         self.assertRedirects(res, "/accounts/login/")
 
     ### Any other tests?
+        
--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -13,13 +13,18 @@ class LoginTestCase(TestCase):
         session = self.client.session
         session["welcome_code"] = str(check.code)
         session.save()
-
+        self.old_user_count = User.objects.count()
         form = {"email": "alice@example.org"}
 
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
 
+        link_confirmation = self.client.get('/accounts/login_link_sent/')
+        self.assertContains(link_confirmation, "Login Link Sent")
+
         ### Assert that a user was created
+        self.user_check = User.objects.filter(email="alice@example.org")
+        self.assertEqual(self.user_check[0].email, form["email"])
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
@@ -27,6 +32,10 @@ class LoginTestCase(TestCase):
         ### Assert contents of the email body
 
         ### Assert that check is associated with the new user
+        user = User.objects.get(email="alice@example.org")
+
+        check_user = Check.objects.get(user=user)
+        self.assertEqual(check_user.user, user)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
