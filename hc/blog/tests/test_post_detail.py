43d113e3002d788b13530e9dc7cd6f6594c264c9
brayovsky <brian.muthui@andela.com>
2017-03-13 18:45:21 +0300

    [feature 141403441] Add tests for editing blog feature

--- hc/blog/tests/test_post_detail.py
+++ hc/blog/tests/test_post_detail.py
@@ -20,4 +20,34 @@ class TestBlogDetail(BaseTestCase):
 
         assert blog_post.status_code == 404
 
+    def test_edit_blog(self):
+        # Set alice to become a superuser in order for her to edit
+        self.alice.is_superuser = True
+        self.alice.is_staff = True
+        self.alice.save()
+
+        self.client.login(username="alice", password="password")
+        # Create a blog
+        post = Post.objects.create(author=self.alice)
+
+        edit_form_page = self.client.get("/blog/post/{0}/edit/".format(post.id))
+        assert edit_form_page.status_code == 200
+
+        bad_edit_form_page = self.client.get("/blog/post/{0}/edit/".format(post.id+1))
+        assert bad_edit_form_page.status_code == 404
+
+        # Submit post that has been edited
+        data = {
+            "title": "Testing",
+            "text": "Life of pie"
+        }
+
+        submitted_edit = self.client.post("/blog/post/{0}/edit/".format(post.id), data)
+        post.refresh_from_db()
+
+        assert post.title == data["title"]
+        assert post.text == data["text"]
+
+    def test_new_blog(self):
+        pass
 
