a4ad31c02950439eefc63f7aaa0ead35840e100a
brayovsky <brian.muthui@andela.com>
2017-03-13 16:27:49 +0300

    [feature 141403441] Add test for an invalid blog page - Remove test for post id parameter in url

--- hc/blog/tests/test_post_detail.py
+++ hc/blog/tests/test_post_detail.py
@@ -8,7 +8,6 @@ class TestBlogDetail(BaseTestCase):
         super(TestBlogDetail, self).setUp()
 
     def test_blog_page_works(self):
-        # Create new blog, access it using url, assert 200
         post = Post.objects.create(author=self.alice)
         blog_post = self.client.get("/blog/post/{0}/".format(post.id))
 
@@ -16,5 +15,9 @@ class TestBlogDetail(BaseTestCase):
 
     def test_invalid_blog_page(self):
         # Create new blog, access +1 param, assert 404
-        pass
+        post = Post.objects.create(author=self.alice)
+        blog_post = self.client.get("/blog/post/{0}/".format(post.id+1))
+
+        assert blog_post.status_code == 404
+
 
