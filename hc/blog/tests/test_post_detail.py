0c881653016ac0120bc0cc6b8ef9e1356740d566
brayovsky <brian.muthui@andela.com>
2017-03-13 19:17:05 +0300

    [feature 141403441] Add tests for creating a new blog feature - Refactor test code to become DRYier

--- hc/blog/tests/test_post_detail.py
+++ hc/blog/tests/test_post_detail.py
@@ -7,6 +7,18 @@ class TestBlogDetail(BaseTestCase):
     def setUp(self):
         super(TestBlogDetail, self).setUp()
 
+        # Set alice to become a superuser in order for her to edit
+        self.alice.is_superuser = True
+        self.alice.is_staff = True
+        self.alice.save()
+
+        self.client.login(username="alice", password="password")
+
+        self.data = {
+            "title": "Testing",
+            "text": "Life of pie"
+        }
+
     def test_blog_page_works(self):
         post = Post.objects.create(author=self.alice)
         blog_post = self.client.get("/blog/post/{0}/".format(post.id))
@@ -21,12 +33,6 @@ class TestBlogDetail(BaseTestCase):
         assert blog_post.status_code == 404
 
     def test_edit_blog(self):
-        # Set alice to become a superuser in order for her to edit
-        self.alice.is_superuser = True
-        self.alice.is_staff = True
-        self.alice.save()
-
-        self.client.login(username="alice", password="password")
         # Create a blog
         post = Post.objects.create(author=self.alice)
 
@@ -37,17 +43,31 @@ class TestBlogDetail(BaseTestCase):
         assert bad_edit_form_page.status_code == 404
 
         # Submit post that has been edited
-        data = {
-            "title": "Testing",
-            "text": "Life of pie"
-        }
+        submitted_edit = self.client.post("/blog/post/{0}/edit/".format(post.id), self.data)
 
-        submitted_edit = self.client.post("/blog/post/{0}/edit/".format(post.id), data)
+        assert submitted_edit.status_code == 302
         post.refresh_from_db()
 
-        assert post.title == data["title"]
-        assert post.text == data["text"]
+        assert post.title == self.data["title"]
+        assert post.text == self.data["text"]
 
     def test_new_blog(self):
-        pass
+        new_blog_page = self.client.get("/blog/new/")
+        assert new_blog_page.status_code == 200
+
+        post = Post.objects.create(author=self.alice)
+        #
+        new_blog = self.client.post("/blog/new/", self.data)
+        assert new_blog.status_code == 302
+
+        # Assert a record was added
+        latest_post = Post.objects.latest("id")
+        assert (post.id+1) == latest_post.id
+        assert latest_post.text == self.data["text"]
+
+
+
+
+
+
 
